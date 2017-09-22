6f4a70129979b27d57900d0bafd459972cb9835a
muhammadjafar <mjafar747@gmail.com>
2017-09-22 13:53:50 +0700

    id unik

--- application/models/register_model.php
+++ application/models/register_model.php
@@ -1,13 +1,26 @@
 <?php
 	class register_model extends CI_Model{
 		public function register(){
-			$data=array('sell_id' => '',
+			$q = $this->db->query("SELECT MAX(RIGHT(sell_id,4)) AS idmax FROM tb_seller");
+			$kd = ""; //kode awal
+			if($q->num_rows()>0){ //jika data ada
+				foreach($q->result() as $k){
+					$tmp = ((int)$k->idmax)+1; //string kode diset ke integer dan ditambahkan 1 dari kode terakhir
+					$kd = sprintf('%04s', $tmp); //kode ambil 4 karakter terakhir
+				}
+			}else{ //jika data kosong diset ke kode awal
+				$kd = "0001";
+			}
+			$kar = "SEL"; //karakter depan kodenya
+			//gabungkan string dengan kode yang telah dibuat tadi
+			$data=array('sell_id' => $kar.$kd,
 						'sell_email' => $this->input->post('email'),
 						'sell_name' => $this->input->post('nama'),
 						'sell_pass' => $this->input->post('pass'),
 						'sell_phone' => $this->input->post('telp'),
 						'sell_status' => 'aktif',
 			);
+			$this->db->set('sell_date_reg', 'NOW()', FALSE);
 			$this->db->insert('tb_seller',$data);
 		}
 	}
